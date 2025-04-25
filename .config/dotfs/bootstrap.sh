424241b73ec97190cee367af7f6ade040ecc0ca2
HubbeKing <github@hubbe.club>
2025-04-25 19:05:57 +0300

    Make bootstrap script idempotent

--- .config/dotfs/bootstrap.sh
+++ .config/dotfs/bootstrap.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
+set -e
 
 # stage updates
 rpm-ostree upgrade
 
 # stage packages from bootstrap list
-packages=$(cat ~/.config/bootstrap/packages.lst)
-rpm-ostree install $packages
+packages=$(cat ~/.config/dotfs/packages.lst)
+rpm-ostree install --idempotent $packages
 
 # enable steam nonfree repo
-sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
+sudo sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
 # stage steam-devices package (needed for steam flatpak)
 rpm-ostree refresh-md
-rpm-ostree install steam-packages
-
-# set up rpmfusion repos
-# https://rpmfusion.org/Howto/OSTree
-sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+rpm-ostree install --idempotent steam-devices
 
 # set up vscodium repo
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 # stage vscodium package
-rpm-ostree install codium
+rpm-ostree install --idempotent codium
 
 # set up k9s copr repo
 wget https://copr.fedorainfracloud.org/coprs/luminoso/k9s/repo/fedora-42/luminoso-k9s-fedora-42.repo
 sudo mv luminoso-k9s-fedora-42.repo /etc/yum.repos.d/
 # stage k9s package
-rpm-ostree install k9s
+rpm-ostree install --idempotent k9s
 
 # set up lact copr repo
 wget https://copr.fedorainfracloud.org/coprs/ilyaz/LACT/repo/fedora-42/ilyaz-LACT-fedora-42.repo
 sudo mv ilyaz-LACT-fedora-42.repo /etc/yum.repos.d/
 # stage lact package
-rpm-ostree install lact
+rpm-ostree install --idempotent lact
 # add AMDGPU kernel arg for overclocking
-rpm-ostree kargs --append=amdgpu.ppfeaturemask=0xffffffff
+rpm-ostree kargs --append-if-missing=amdgpu.ppfeaturemask=0xffffffff
 
 # install flatpaks from bootstrap list
-flatpaks=$(cat ~/.config/bootstrap/flatpaks.lst)
-flatpak install $flatpaks
+flatpaks=$(cat ~/.config/dotfs/flatpaks.lst)
+flatpak install --or-update $flatpaks
 
 # install OrcaSlicer
 wget https://github.com/SoftFever/OrcaSlicer/releases/download/v2.3.0/OrcaSlicer-Linux-flatpak_V2.3.0_x86_64.flatpak
-flatpak install ./OrcaSlicer-Linux-flatpak_V2.3.0_x86_64.flatpak
+flatpak install --or-update ./OrcaSlicer-Linux-flatpak_V2.3.0_x86_64.flatpak
+
+# set up rpmfusion repos
+# this needs to be done last, before reboot, because it breaks the md-refresh steps of rpm-ostree
+# https://rpmfusion.org/Howto/OSTree
+sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 echo "Bootstrap complete - Please reboot system, and then run .config/dotfs/post-reboot.sh"
