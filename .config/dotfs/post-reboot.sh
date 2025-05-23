4042cefe020ece442834eae919a5fc36b29c4959
HubbeKing <github@hubbe.club>
2025-05-23 10:56:16 +0300

    Remove lactd from post-reboot script

--- .config/dotfs/post-reboot.sh
+++ .config/dotfs/post-reboot.sh
@@ -8,6 +8,3 @@ sudo rpm-ostree update --uninstall rpmfusion-free-release --uninstall rpmfusion-
 rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
 rpm-ostree install gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi
 rpm-ostree override remove libavcodec-free libavdevice-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free ffmpeg-free --install ffmpeg
-
-# enable lactd
-sudo systemctl enable --now lactd
